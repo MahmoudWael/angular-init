cd5e8478f0bd81c27fd7b29aa52ffcb37c303286
mahmoudwael <mxheond9@gmail.com>
2019-02-14 20:42:26 +0200

    products component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'searchX'`, () => {
+  it(`should have as title 'angular-init'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('searchX');
+    expect(app.title).toEqual('angular-init');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to searchX!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-init!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'searchX';
+  title = 'Angular init app';
 }
